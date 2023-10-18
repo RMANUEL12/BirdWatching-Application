3447ca9f83b54de6c5de2726d5f799788d30c7c3
jayymmanuel <emmanuelkianda@gmail.com>
2023-10-18 19:45:16 +0200

    Ui Updates

--- app/src/main/java/com/example/bobolinkbirdwatching/Register.kt
+++ app/src/main/java/com/example/bobolinkbirdwatching/Register.kt
@@ -10,7 +10,7 @@ import android.widget.EditText
 import android.widget.Toast
 
 class Register : AppCompatActivity() {
-    private var buttonRegister = Button(this);
+    private lateinit var buttonRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +20,8 @@ class Register : AppCompatActivity() {
 
         buttonRegister.setOnClickListener {
 
-            try{
-                //storing user input
+            try {
+                // Storing user input
                 val email = findViewById<EditText>(R.id.editTextEmail).text.toString()
                 val passwordTemp = findViewById<EditText>(R.id.editTextPassword).text.toString()
                 val password = findViewById<EditText>(R.id.editTextConfirmPassword).text.toString()
@@ -29,43 +29,35 @@ class Register : AppCompatActivity() {
                 val surname = findViewById<EditText>(R.id.editTextSurname).text.toString()
                 val area = findViewById<EditText>(R.id.editTextArea).text.toString()
 
-
-
-            //Adding the user to the list
-            com.example.bobolinkbirdwatching.User.addUser(newUser)
-
-                //verifying that no fields are empty
+                // Verifying that no fields are empty
                 if (email.isNotEmpty() && password.isNotEmpty() && firstName.isNotEmpty() &&
-                    surname.isNotEmpty() && area.isNotEmpty()){
+                    surname.isNotEmpty() && area.isNotEmpty()) {
 
+                    // Checking that password and confirmed password match
+                    if (passwordTemp == password) {
 
-                    //checking that password and confirmed password match
-                    if (passwordTemp == password){
-
-                        //Creating a new user
+                        // Creating a new user
                         val newUser = UserData(firstName, surname, area, email, password)
 
-                        //Adding the user to the list
+                        // Adding the user to the list
                         com.example.bobolinkbirdwatching.User.addUser(newUser)
 
-                        //--REDIRECTING TO LOGIN
-                        //Creating an Intent to navigate to the activity
+                        // Redirecting to login
+                        // Creating an Intent to navigate to the activity
                         val intent = Intent(this, Login::class.java)
 
-                        //Starting the activity
+                        // Starting the activity
                         startActivity(intent)
 
-                    }
-                    else{
+                    } else {
                         // Showing error message
                         Toast.makeText(applicationContext, "Please ensure that both passwords match.", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
-            //error handling
-            catch(e: Exception){
-
-                //error message
+            // Error handling
+            catch (e: Exception) {
+                // Error message
                 Log.e(ContentValues.TAG, "Something went wrong, Please Try Again.")
             }
         }
