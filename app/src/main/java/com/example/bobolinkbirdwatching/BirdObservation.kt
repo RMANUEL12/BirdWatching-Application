5bad672796e900055a320d59f0a725676bd859ca
ST10081690 <103584069+ST10081690@users.noreply.github.com>
2023-10-18 14:17:58 +0200

    Bird Observation Activities + Error Handling Updates

---
+++ app/src/main/java/com/example/bobolinkbirdwatching/BirdObservation.kt
@@ -0,0 +1,8 @@
+package com.example.bobolinkbirdwatching
+
+data class BirdObservation (
+    val breed: String,
+    val sightLocation: String,
+    val dateSpotted: String,
+
+)
--- app/src/main/java/com/example/bobolinkbirdwatching/Login.kt
+++ app/src/main/java/com/example/bobolinkbirdwatching/Login.kt
@@ -1,7 +1,9 @@
 package com.example.bobolinkbirdwatching
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -16,17 +18,26 @@ class Login : AppCompatActivity() {
         btnLogin = findViewById(R.id.buttonLogin)
 
         btnLogin.setOnClickListener {
-            val email = findViewById<EditText>(R.id.inputEmail).text.toString()
-            val password = findViewById<EditText>(R.id.inputPassword).text.toString()
+            try {
+                val email = findViewById<EditText>(R.id.inputEmail).text.toString()
+                val password = findViewById<EditText>(R.id.inputPassword).text.toString()
 
-            val user =
-                com.example.bobolinkbirdwatching.User.findUserByEmailAndPassword(email, password)
-            if (user != null) {
-                // User will be directed to home page
-            } else {
-                // Showing message for invalid user details
-                Toast.makeText(applicationContext, "User not found. Please Register...", Toast.LENGTH_SHORT).show()
+                val user =
+                    com.example.bobolinkbirdwatching.User.findUserByEmailAndPassword(email, password)
+                if (user != null) {
+                    // User will be directed to home page
+                } else {
+                    // Showing message for invalid user details
+                    Toast.makeText(applicationContext, "Invalid Details. Please Retry or Register...", Toast.LENGTH_SHORT).show()
+                }
             }
+            //error handling
+            catch(e: Exception){
+
+                //error message
+                Log.e(ContentValues.TAG, "Something went wrong, Please Try Again.")
+            }
+
         }
     }
 }
